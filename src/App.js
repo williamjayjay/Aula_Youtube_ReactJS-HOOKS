e662a017e43589c6fc5d4efa88e9845789bebc6e
William Gomes <williamdn@yahoo.com>
2021-01-08 06:04:14 -0300

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [notas, setNotas] = useState([
+    'aprender ReactJS',
+    'aprender React Native'
+  ])
+
+  const [input, setInput] = useState('')
+
+  function addNotes(){
+    // alert('notas add!!')
+    if(input != ''){
+      setNotas([...notas, input])
+      setInput('')
+    }else{
+      alert('Por favor digite um valor no campo.')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     {/* <h1>{notas}</h1> */}
+     <ul>
+       {notas.map(notinhas => (
+         <li key={notinhas} >{notinhas}</li>
+       ))}
+     </ul>
+
+     <input type="text" value={input} onChange={event => setInput(event.target.value)} />
+     <button type="button" onClick={addNotes} >ADD Notes</button>
+
+
+   </div>
   );
 }
 
